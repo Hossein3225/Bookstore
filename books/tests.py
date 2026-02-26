9fad82b3eb27d9207dc054829070deb2f7ed87b6
Jamshid3225 <rahmanitkd@gmail.com>
2026-02-26 15:42:24 +0330

    add review sec to the bookstore. ch12 of book

--- books/tests.py
+++ books/tests.py
@@ -1,17 +1,28 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 # Create your tests here.
 
-from .models import Book
+from .models import Book , Review
 
 class BookTests(TestCase):
     @classmethod
     def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="reviewuser",
+            email = "reviewuser@emaol.com",
+            password="testpass123",
+        )
         cls.book = Book.objects.create(
             title = "Shahname",
             author = "Ferdowsi",
             price = "12.00",
             )
+        cls.review= Review.objects.create(
+            book = cls.book,
+            author = cls.user,
+            review = "this is a great review"
+        )
     
     def test_book_listing(self):
         self.assertEqual(self.book.title , "Shahname")
@@ -30,4 +41,5 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code , 200)
         self.assertEqual(no_response.status_code,404)
         self.assertContains(response , "Shahname")
+        self.assertContains(response, "this is a great review")
         self.assertTemplateUsed(response,"books/book_detail.html")
